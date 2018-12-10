e485b909e92c4c0a94f6eff3b4ece5d498dfc24d
adam-hotait <adam.hotait@student.ecp.fr>
2018-12-10 13:50:37 +0100

    finish index implentation on CACM

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import sys
 import re
+import math
+from matplotlib import pyplot as plt
+import scipy.stats
 
 index = dict()
 common_words = set()
+freq = dict()
 
 token = 0
+
+
+logT = []
+logM = []
 
 with open("data/CACM/common_words", "r") as common_words_list:
     for word in common_words_list:
@@ -16,11 +24,23 @@ def traiter_ligne(docID, line):
     token = 0
     for word in words:
         token += 1
+
         if word not in common_words:
+
+            # maj frequence
             try:
-                index[word].add(docID)
+                freq[word] += 1
             except KeyError:
-                index[word] = {docID}
+                freq[word] = 1
+
+            # maj indexß
+            try:
+                if docID in index[word]:
+                    index[word][docID] += 1
+                else:
+                    index[word][docID] = 1
+            except KeyError:
+                index[word] = {docID: 1}
     return token
 
 reading = False
@@ -36,11 +56,28 @@ while True:
         reading = False
     elif reading:
         token += traiter_ligne(docID, line)
+        logT.append(math.log(token, 10))
+        logM.append(math.log(len(index), 10))
 
-for word, value in index.items():
-    index[word] = sorted(list(value))
 
-print(index)
+for word, dicoDoc in index.items():
+    index[word] = [(docID, frequency) for docID, frequency in dicoDoc.items()]
+    index[word].sort()
 
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(logT[len(logT)//2:], logM[len(logM)//2:])
+
+b = slope
+k = 10**intercept
 print("Il y a {} tokens dans la collection.".format(token))
 print("Il y a {} mots dans le vocabulaire.".format(len(index)))
+print("b: ", b)
+print("log(k): ", intercept)
+print('k: ', k)
+print("Pour 1 000 000 de tokens, il y aurait {} mots de vocabulaire.".format(int(k*10e6**b)))
+# plt.plot(logT, logM)
+# plt.show()
+
+# for word, docIDs in index.items():
+#     print(type(docIDs))
+#     break
+
